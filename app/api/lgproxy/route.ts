66eba2ee41a9af6f274c5a12708d5629e7611cc2
Irsyad Khoirul Anwar <109755636+irsyadanwar@users.noreply.github.com>
2026-02-23 00:50:09 +0100

    Update lgproxy URL in POST request

--- app/api/lgproxy/route.ts
+++ app/api/lgproxy/route.ts
@@ -4,7 +4,7 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function POST(req: NextRequest) {
   const { command, endpoint } = await req.json();
   // endpoint: '/bird', '/bird6', '/traceroute', '/traceroute6'
-  const url = `http://103.254.106.158:5000${endpoint}?q=${encodeURIComponent(command)}`;
+  const url = `http://103.254.104.35:5000${endpoint}?q=${encodeURIComponent(command)}`;
   const res = await fetch(url, {
     method: 'GET',
     headers: {
